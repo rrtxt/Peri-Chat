d2f27ac0a5a9b8e836c9d9a742786512b677d282
rrtxt <rizfiferdian1@gmail.com>
2025-07-28 15:29:11 +0700

    change search tool max result into 3

--- src/lib/tools/searchTools.ts
+++ src/lib/tools/searchTools.ts
@@ -1,7 +1,12 @@
-import { TavilySearch } from "@langchain/tavily";
+import { TavilyExtract, TavilySearch } from "@langchain/tavily";
 
 export const tavilySearchTool = new TavilySearch({
-  maxResults: 5,
+  maxResults: 3,
   topic: "general",
+  tavilyApiKey: process.env.TAVILY_API_KEY || "",
+  includeRawContent: true,
+});
+
+export const tavilyExtractTool = new TavilyExtract({
   tavilyApiKey: process.env.TAVILY_API_KEY || "",
 });
